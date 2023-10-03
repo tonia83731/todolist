10c71a98cea74cb9161907878c853ba258b14be9
tonia94731 <huiyu.tonia.chang@gmail.com>
2023-10-03 13:42:08 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import exphbs from 'express-handlebars'
+import bodyParser from 'body-parser';
+import Todo from './models/todo.js';
 
 if(process.env.NODE_ENV !== "production") {
   dotenv.config();
@@ -22,12 +24,29 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
-app.set('view engine', hbs)
+app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   // res.send('hello world')
-  res.render('index')
+  // res.render('index')
+  Todo.find()
+    .lean()
+    .then(todos => res.render('index', {todos}))
+    .catch(error => console.error(error))
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  return Todo.create({name})
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
